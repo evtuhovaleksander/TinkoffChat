f6d53f4a7f4dfd23f1487b827b87d70fe56d7014
Evtuhov <Evtuhov@binbank.ru>
2017-09-24 14:19:49 +0300

    init

---
+++ TinkoffChat/TinkoffChat/ProfileViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  TinkoffChat
+//
+//  Created by Aleksander Evtuhov on 20/09/2017.
+//  Copyright © 2017 Aleksander Evtuhov. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //view.backgroundColor = .green
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
