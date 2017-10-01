c585a242e353b7221b75c1b83ad2f1f8accb5a88
Evtuhov <Evtuhov@binbank.ru>
2017-10-01 15:28:17 +0300

    form setup

--- TinkoffChat/TinkoffChat/ProfileViewController.swift
+++ TinkoffChat/TinkoffChat/ProfileViewController.swift
@@ -12,10 +12,28 @@ class ProfileViewController: UIViewController {
 
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var infoLabel: UILabel!
-    
+
+    @IBOutlet weak var editProfileButton: UIButton!
+    @IBOutlet weak var photoImageView: UIImageView!
+
+    @IBOutlet weak var makePhotoButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         printFuncName()
+//      setup corner radius
+        photoImageView.layer.cornerRadius = 50
+        makePhotoButton.layer.cornerRadius = 50
+        let offsetConstant = CGFloat(20)
+        makePhotoButton.imageEdgeInsets = UIEdgeInsetsMake(offsetConstant, offsetConstant, offsetConstant, offsetConstant)
+        photoImageView.clipsToBounds = true
+        makePhotoButton.clipsToBounds = true
+//      setup data
+        nameLabel.text = "Alex Dark"
+        infoLabel.text = "Hello! I'm Alex Dark, and you can be my best friend!"
+//      setup button
+        editProfileButton.layer.borderWidth = 1
+        editProfileButton.layer.borderColor = UIColor.black.cgColor
     }
 
     override func didReceiveMemoryWarning() {
@@ -41,6 +59,7 @@ class ProfileViewController: UIViewController {
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         printFuncName()
+
     }
 
     override func viewWillDisappear(_ animated: Bool) {
