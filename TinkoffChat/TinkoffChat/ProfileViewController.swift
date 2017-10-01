aec6a665532b8ef2e5511a1e53fe4cbf8c9f9e38
Evtuhov <Evtuhov@binbank.ru>
2017-10-01 14:59:53 +0300

    конец второй лекции

--- TinkoffChat/TinkoffChat/ProfileViewController.swift
+++ TinkoffChat/TinkoffChat/ProfileViewController.swift
@@ -12,14 +12,6 @@ class ProfileViewController: UIViewController {
 
     @IBOutlet weak var nameLabel: UILabel!
 
-    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
-        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
-    }
-
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
     override func viewDidLoad() {
         super.viewDidLoad()
         printFuncName()
